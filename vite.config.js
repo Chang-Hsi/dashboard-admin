cda3c5e686d16d621abaf2cc53ba07ed5b1bc678
Chang-Hsi <watasiwa8531@gmail.com>
2025-11-24 16:55:59 +0800

    chore: set base for github pages

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,14 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/dashboard-admin/', // 👈 GitHub Pages 子路徑
   plugins: [
     vue(),
+    vueDevTools(),
     tailwindcss(),
   ],
   resolve: {
